cbb8619a55aa5407bfce88b6b9329a439439c663
Ivan Brazza <dudeman1996@gmail.com>
2012-09-19 16:08:36 +0100

    Add upload build to my blog via scp

--- build.sh
+++ build.sh
@@ -124,6 +124,10 @@ then
   echo ""
   echo "${bldblu}Uploading to goo.im...${txtrst}"
   scp out/target/product/marvel/cm-10-"$date"-EXPERIMENTAL-marvel-CYANOGEN-NIGHTLY.zip dudeman1996@upload.goo.im:public_html/CM10-Nightlies_marvel/cm-10-"$date"-EXPERIMENTAL-marvel-NIGHTLY.zip
+  echo ""
+  echo "${bldblu}Uploading to mirror...${txtrst}"
+  scp out/target/product/marvel/cm-10-"$date"-EXPERIMENTAL-marvel-CYANOGEN-NIGHTLY.zip dudeman1996@75.127.15.79:rommanager/cm-10-"$date"-EXPERIMENTAL-marvel-NIGHTLY.zip
+  echo ""
   res3=$(date -u +%s)
 elif [ "$upload" = "false" ]
 then
